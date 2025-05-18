7f8abfac2374d96fec628a67fb3760034dbe1927
Orlando <espadachinkinno@gmail.com>
2025-05-17 20:31:20 -0600

    feat: update Header component layout and styles; modify logo size in SVG

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,14 @@
+import logo from '../assets/logo.svg';
 import styles from './Header.module.css';
 
 export const Header = () => {
   return (
     <>
         <header className={styles.header}>
-            <h1>MoonSleep</h1>
+            <div className={styles.logo}>
+                <img src={logo} alt="MoonSleep Logo" />
+                <h1>MoonSleep</h1>
+            </div>
             <button>Perfil</button>
         </header>
     </>
