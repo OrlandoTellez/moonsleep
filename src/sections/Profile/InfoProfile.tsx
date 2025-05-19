369eb978e27edbd2a7d7f953804295bb4159c8ea
Orlando <espadachinkinno@gmail.com>
2025-05-18 21:01:20 -0600

    feat: update InfoProfile component; integrate Button for login functionality and adjust imports

--- src/sections/Profile/InfoProfile.tsx
+++ src/sections/Profile/InfoProfile.tsx
@@ -1,8 +1,11 @@
-import styles from './InfoProfile.module.css';
 import { Icon } from '../../components/Icon';
+import { Button } from '../../components/Button';
+import { useAuth0 } from '@auth0/auth0-react';
 import user from '../../assets/user.svg';
+import styles from './InfoProfile.module.css';
 
 export const InfoProfile = () => {
+  const { loginWithRedirect } = useAuth0();
   return (
     <>
         <div className={styles.container}>
@@ -13,6 +16,13 @@ export const InfoProfile = () => {
             <article>
               <h1>Tu perfil</h1>
             </article>
+
+            <div>
+              <Button 
+              text='Iniciar sesión'
+              onClick={() => loginWithRedirect()}
+              />
+            </div>
         </div>
     </>
   )
