d2f5c86c0d4596b05b3338bb68fdd078e4429007
Orlando <espadachinkinno@gmail.com>
2025-05-18 21:18:28 -0600

    feat: enhance InfoProfile component; update user icon handling and add conditional rendering for authentication state

--- src/sections/Profile/InfoProfile.tsx
+++ src/sections/Profile/InfoProfile.tsx
@@ -1,27 +1,45 @@
-import { Icon } from '../../components/Icon';
-import { Button } from '../../components/Button';
-import { useAuth0 } from '@auth0/auth0-react';
-import user from '../../assets/user.svg';
-import styles from './InfoProfile.module.css';
+import { Icon } from '../../components/Icon'
+import { Button } from '../../components/Button'
+import { useAuth0 } from '@auth0/auth0-react'
+import userSvg from '../../assets/user.svg'
+import styles from './InfoProfile.module.css'
 
 export const InfoProfile = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0()
   return (
     <>
         <div className={styles.container}>
             <Icon 
-            icon={user} 
+            icon={userSvg} 
             alt='user icon'
             />      
-            <article>
-              <h1>Tu perfil</h1>
-            </article>
+            {
+              isAuthenticated ? (
+                <article>
+                  <h1>{user?.name}</h1>
+                </article>
+              ) : (
+                <article>
+                  <h1>Tu perfil</h1>
+                </article>
+              )
+            }
+            
 
             <div>
-              <Button 
-              text='Iniciar sesión'
-              onClick={() => loginWithRedirect()}
-              />
+              {
+                isAuthenticated ? (
+                  <Button
+                  text='Cerrar sesión'
+                  onClick={() => logout()}
+                  />
+                ) : (
+                  <Button 
+                  text='Iniciar sesión'
+                  onClick={() => loginWithRedirect()}
+                  />
+                )
+              }
             </div>
         </div>
     </>
