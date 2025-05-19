c21a801e1672e7e2fd95b419a325ab62d1830e64
Orlando <espadachinkinno@gmail.com>
2025-05-18 20:52:19 -0600

    feat: integrate Auth0 for authentication; remove padding from Profile navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import { BrowserRouter as Router } from 'react-router-dom'
+import { Auth0Provider } from '@auth0/auth0-react'
 import { AppRoutes } from './routes/AppRoutes'
+
+const DOMAIN = import.meta.env.VITE_AUTH0_DOMAIN
+const CLIENT_ID = import.meta.env.VITE_AUTH0_CLIENT_ID
 
 function App() {
   return (
     <>
-      <Router>
-        <AppRoutes />
-      </Router>
+      <Auth0Provider
+      domain={DOMAIN}
+      clientId={CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}>
+        <Router>
+          <AppRoutes />
+        </Router>
+      </Auth0Provider>
     </>
   )
 }
